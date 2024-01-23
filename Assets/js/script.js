b1e40ab2d09cf92e9cdaf9ff586ba837ee888f92
Levangul <150108077+Levangul@users.noreply.github.com>
2024-01-23 02:43:29 -0500

    readme filepath maincode

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,29 +3,41 @@ $(function () {
 
   function updateTime() {
     let todayDate = dayjs()
-    $("#currentDay").text(todayDate.format("dddd MMMM Do h:mm:ss"));
-    let now = dayjs().hour('H')
+    $("#currentDay").text(todayDate.format("dddd MMMM D - h:mm:ss"));
+    let currentHour = dayjs().format('H')
 
     $('.time-block').each(function () {
+
+      //* converting string into integar
+
       let timeBlock = parseInt(this.id);
-      if (timeBlock == now) {
-        $(this).removeClass('past future').addClass('present');
-      } else if (timeBlock < now) {
-        $(this).removeClass('future present').addClass('past');
+
+      //* adding classes depending on hour to switch colors
+
+      if (timeBlock == currentHour) {
+        $(this).addClass('present');
+      } else if (timeBlock < currentHour) {
+        $(this).addClass('past');
       } else {
-        $(this).removeClass('past present').addClass('future');
+        $(this).addClass('future');
       }
     })
   }
   function saveText() {
+
+    //* function to set value into local storage after click
+
     $('.saveBtn').on('click', function () {
       let key = $(this).parent().attr('id');
       let value = $(this).siblings('.description').val();
       localStorage.setItem(key, value);
     });
-
+    
   }
   $('.time-block').each(function () {
+
+    //* function to get value from local storage
+
     key = $(this).attr('id');
     value = localStorage.getItem(key);
     $(this).children('.description').val(value);
@@ -34,24 +46,5 @@ $(function () {
   updateTime()
   setInterval(updateTime, 1000)
   saveText()
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 });
+
